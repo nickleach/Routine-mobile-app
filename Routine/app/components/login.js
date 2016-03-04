2cc77fffc6e6d58f12a6f50a5d88651f680505ea
Cori Fowler <fowler.cori@gmail.com>
2016-03-04 13:10:09 -0500

    added email and password input fields to login view

--- Routine/app/components/login.js
+++ Routine/app/components/login.js
@@ -1,20 +1,28 @@
-import React, { Component, StyleSheet, Text, View } from 'react-native';
+import React, { Component, StyleSheet, Text, View, TextInput } from 'react-native';
 import * as Colors from '../modules/colors';
 
 const
     styles = StyleSheet.create({
+        textInput: {
+            height: 40,
+            width: 300,
+            borderColor: '#333333',
+            borderBottomWidth: 20
+        }
     }),
 
 _component = React.createClass({
     statics: {
-        title: '',
+        title: 'Login',
         description: ''
     },
-    displayName: '',
+    displayName: 'Login',
     propTypes: {
     },
     getInitialState() {
         return {
+            email: 'E-mail Address',
+            password: 'Password'
         };
     },
     componentDidMount() {
@@ -24,7 +32,25 @@ _component = React.createClass({
     render() {
         return (
             <View>
-                <Text>default template</Text>
+                <Text>Routine</Text>
+                <View
+                    style={{borderBottomColor: '#333333', borderBottomWidth: 1}}>
+                    <TextInput
+                        style={styles.textInput}
+                        onChangeText={(email) => this.setState({email})}
+                        placeholder={this.state.email}
+                        value={this.state.email}
+                      />
+                </View>
+                <View
+                    style={{borderBottomColor: '#333333', borderBottomWidth: 1}}>
+                    <TextInput
+                        secureTextEntry={true}
+                        style={styles.textInput}
+                        onChangeText={(password) => this.setState({password})}
+                        value={this.state.password}
+                      />
+                </View>
             </View>
         );
     }
