284a250b1fa0f2882467f4e1160ade2b000dd3da
Cori Fowler <fowler.cori@gmail.com>
2016-03-04 15:10:27 -0500

    added facebook login button and login button

--- Routine/app/components/login.js
+++ Routine/app/components/login.js
@@ -1,15 +1,45 @@
-import React, { Component, StyleSheet, Text, View, TextInput } from 'react-native';
+import React, { Component, StyleSheet, Text, View, TextInput, TouchableHighlight, Image } from 'react-native';
 import * as Colors from '../modules/colors';
 
 const
     styles = StyleSheet.create({
+        welcome: {
+            fontSize: 55,
+            alignSelf: 'center',
+            marginTop: 75,
+            marginBottom: 50
+        },
         textInput: {
             height: 40,
             width: 300,
             borderColor: '#333333',
             borderBottomWidth: 20
+        },
+        inputBorder: {
+            borderBottomColor: '#333333',
+            borderBottomWidth: 1
+        },
+        button: {
+            width: 300,
+            backgroundColor: '#333333',
+            color: 'white',
+            height: 40,
+            textAlign: 'center',
+            marginTop: 25,
+            paddingTop: 10
+        },
+        or: {
+            alignSelf: 'center',
+            marginTop: 40,
+            marginBottom: 40,
+            fontSize: 20
+        },
+        image: {
+            width: 300,
+            height: 40,
         }
     }),
+    facebookLogin = {uri: '../assets/facebookLogin.png'},
 
 _component = React.createClass({
     statics: {
@@ -21,9 +51,13 @@ _component = React.createClass({
     },
     getInitialState() {
         return {
+            welcome: 'Routine',
             email: 'E-mail Address',
             password: 'Password'
         };
+    },
+    onLogin() {
+        console.log('pressing login button');
     },
     componentDidMount() {
     },
@@ -32,9 +66,12 @@ _component = React.createClass({
     render() {
         return (
             <View>
-                <Text>Routine</Text>
+                <Text
+                    style={styles.welcome}>
+                    {this.state.welcome}
+                </Text>
                 <View
-                    style={{borderBottomColor: '#333333', borderBottomWidth: 1}}>
+                    style={styles.inputBorder}>
                     <TextInput
                         style={styles.textInput}
                         onChangeText={(email) => this.setState({email})}
@@ -43,7 +80,7 @@ _component = React.createClass({
                       />
                 </View>
                 <View
-                    style={{borderBottomColor: '#333333', borderBottomWidth: 1}}>
+                    style={styles.inputBorder}>
                     <TextInput
                         secureTextEntry={true}
                         style={styles.textInput}
@@ -51,6 +88,21 @@ _component = React.createClass({
                         value={this.state.password}
                       />
                 </View>
+                <TouchableHighlight
+                    underlayColor="#a9a9a9"
+                    onPress={this.onLogin()}>
+                    <Text
+                        style={styles.button}>
+                        Login
+                    </Text>
+                </TouchableHighlight>
+                <Text
+                    style={styles.or}>
+                    or
+                </Text>
+                <Image
+                    style={styles.image}
+                    source={require('../assets/facebooklogin.png')} />
             </View>
         );
     }
