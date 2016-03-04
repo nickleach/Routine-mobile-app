45e6fef5ec1ed0a97eba384424a2673a974c5475
Justin <justinowenjacot@icloud.com>
2016-03-04 15:34:17 -0500

    added service api funcitonality

--- Routine/app/components/login.js
+++ Routine/app/components/login.js
@@ -1,33 +1,28 @@
-import React, { Component, StyleSheet, Text, View } from 'react-native';
+import React, { Component, StyleSheet, Text, View, Image } from 'react-native';
 import * as Colors from '../modules/colors';
+import * as ServiceApi from '../modules/serviceApi';
 
 const
     styles = StyleSheet.create({
     }),
-
-_component = React.createClass({
-    statics: {
-        title: '',
-        description: ''
-    },
-    displayName: '',
-    propTypes: {
-    },
-    getInitialState() {
-        return {
-        };
-    },
-    componentDidMount() {
-    },
-    componentDidUpdate(prevProps, prevState) {
-    },
-    render() {
-        return (
-            <View>
-                <Text>default template</Text>
-            </View>
-        );
-    }
-});
+    _component = React.createClass({
+        statics: {
+            title: '',
+            description: ''
+        },
+        displayName: '',
+        propTypes: {
+        },
+        getInitialState() {
+            return {
+            };
+        },
+        componentDidMount() {
+        },
+        componentDidUpdate(prevProps, prevState) {
+        },
+        render() {
+        }
+    });
 
 export default _component;
